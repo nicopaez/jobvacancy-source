3bc21ced80d702b57e0d580a3d99fdc5c1377123
Nicolas Paez <nicopaez@gmail.com>
2015-10-11 19:59:13 -0300

    JobApplication feature added

---
+++ src/main/java/com/jobvacancy/web/rest/JobApplicationResource.java
@@ -0,0 +1,7 @@
+package com.jobvacancy.web.rest;
+
+/**
+ * Created by nicopaez on 10/11/15.
+ */
+public class JobApplicationResource {
+}
---
+++ src/main/java/com/jobvacancy/web/rest/dto/JobApplicationDTO.java
@@ -0,0 +1,7 @@
+package com.jobvacancy.web.rest.dto;
+
+/**
+ * Created by nicopaez on 10/11/15.
+ */
+public class JobApplicationDTO {
+}
